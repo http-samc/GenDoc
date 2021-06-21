65bc02514f793b6bd6256b3cf9640787e74791de
therealbaljeet <chitgopekarsamarth@gmail.com>
2021-06-21 02:32:21 -0500

    Revert "broke the venv"
    This reverts commit f881e255444d57cb05d5a0bd6d119790e19e8512.

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-"""
- setup.py 5/6/2021
-
- MIT License
-
- Copyright (c) 2021 http-samc
-
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
-
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
-
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
-"""
-
 from setuptools import setup
 from os import path
 
@@ -33,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="GenDoc",
-    version="0.0.96",
+    version="0.0.95",
     description="Generate a Markdown Documentation file from a Python Repository with DocStrings.",
     url="https://github.com/http-samc/GenDoc",
     author="Samarth Chitgopekar",
